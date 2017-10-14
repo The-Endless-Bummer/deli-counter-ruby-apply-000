9dd3ba9055ccdc612571f3c06e47d3d83d04aa12
Maurice Roy <mautayro@gmail.com>
2017-10-14 17:38:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,21 +1,22 @@
 katz_deli = []
 
-def line(arr)
-  if arr.empty?
+def line(deli)
+  if deli.empty?
     puts "The line is currently empty."
   else
     in_line = "The line is currently:"
-    arr.each do |name|
-      in_line << " #{arr.index(name) + 1}. #{name}"
+    deli.each do |name|
+      in_line << " #{deli.index(name) + 1}. #{name}"
     end
     puts in_line
   end
 end
 
-def take_a_number(katz_deli, name)
-
+def take_a_number(deli, name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.index(name) + 1} in line."
 end
 
 def now_serving
-
+  
 end
