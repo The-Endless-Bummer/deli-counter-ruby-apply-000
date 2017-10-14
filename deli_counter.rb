e2ddf492ddc2babab0291f7bd922b1164d9346b5
Maurice Roy <mautayro@gmail.com>
2017-10-14 17:41:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,5 +18,9 @@ def take_a_number(deli, name)
 end
 
 def now_serving(deli)
-  puts "Currently serving #{deli.pop}."
+  if deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli.pop}."
+  end
 end
