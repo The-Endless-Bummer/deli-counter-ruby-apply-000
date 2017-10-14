7bedff89263364c67c5daaa09de0bc16c7697eac
Maurice Roy <mautayro@gmail.com>
2017-10-14 17:27:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 
 def line
   if katz_deli.empty?
-    puts "The line is currently empty."
+    "The line is currently empty."
   else
     in_line = "The line is currently\\:"
     katz_deli.each do |name|
